4f8d3b939cf336576572e2b2c45b9a89726a0b04
Poxios <poxios0310@gmail.com>
2022-07-18 17:21:47 +0900

    need background image fit to box

--- common/components/MainPage/Masthead.tsx
+++ common/components/MainPage/Masthead.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 import React from "react";
+import BgImg from '../../assets/img/header-bg.jpg'
 
 const Masthead = () => {
   return (
     <header className="masthead">
+      <Image
+        layout="fill"
+        className="object-center object-cover pointer-events-none"
+        src={BgImg}
+        alt={"Background Image"}
+      />
       <div className="container">
         <div className="masthead-subheading">Welcome To Our Studio!</div>
         <div className="masthead-heading text-uppercase">It&apos;s Nice To Meet You</div>
